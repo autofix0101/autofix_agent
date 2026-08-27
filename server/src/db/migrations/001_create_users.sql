e3f1034e894f0bf45cbc6e769382b176b29a8441
akshay-820 <akshay200583@gmail.com>
2026-08-27 19:16:35 +0530

    Pool initialisation and user schema implemented

---
+++ server/src/db/migrations/001_create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    github_id BIGINT NOT NULL UNIQUE,
+    github_username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    avatar_url VARCHAR(255),
+    github_access_token TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
